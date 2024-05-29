150b94e55809b9abb77992817bc13400fe9049fa
unknown <moisesjuniorcas20@gmail.com>
2024-05-28 21:34:08 -0500

    estructurando homepage

--- src/main/java/org/example/elements/pages/DemoBlazeHomePage.java
+++ src/main/java/org/example/elements/pages/DemoBlazeHomePage.java
@@ -1,4 +1,17 @@
 package org.example.elements.pages;
 
-public class DemoBlazeHomePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+public class DemoBlazeHomePage extends BasePage {
+    private By productLink = By.linkText("samsung galaxy s6"); //se crea un objeto By que contiene el link del producto
+
+    public DemoBlazeHomePage(WebDriver driver) {
+        super(driver);
+
+    }
+
+    public void goToProductPage() {
+        driver.findElement(productLink).click(); //se busca el link del producto y se hace click
+    }
+
 }
