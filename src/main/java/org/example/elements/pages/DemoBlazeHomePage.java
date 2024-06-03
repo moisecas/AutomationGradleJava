55129cacb96daedbf29aecc2e26748da153b5e30
unknown <moisesjuniorcas20@gmail.com>
2024-06-03 15:27:01 -0500

    corrección para agregar producto

--- src/main/java/org/example/elements/pages/DemoBlazeHomePage.java
+++ src/main/java/org/example/elements/pages/DemoBlazeHomePage.java
@@ -2,8 +2,14 @@ package org.example.elements.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class DemoBlazeHomePage extends BasePage { //extiende de la clase BasePage porque es una pagina web y hereda sus metodos de inicializacion de elementos
-    private By productLink = By.linkText("Samsung galaxy s6"); //se crea un objeto By que contiene el link del producto
+    private By productLink = By.xpath("//a[normalize-space()='Samsung galaxy s6']"); //se crea un objeto By que contiene el link del producto (Samsung galaxy s6
 
     public DemoBlazeHomePage(WebDriver driver) { //constructor de la clase
         super(driver); //se llama al constructor de la clase padre que es BasePage y se le pasa el driver
@@ -11,7 +17,9 @@ public class DemoBlazeHomePage extends BasePage { //extiende de la clase BasePag
     }
 
     public void goToProductPage() {
-        driver.findElement(productLink).click(); //se busca el link del producto y se hace click
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Espera de hasta 10 segundos
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(productLink));
+        element.click(); //se hace click en el link del producto
     }
 
 }
