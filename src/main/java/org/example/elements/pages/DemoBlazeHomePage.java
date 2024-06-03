5ca50b6eb094376d81e0f0c87b5d4affacdcfd25
unknown <moisesjuniorcas20@gmail.com>
2024-06-03 14:30:37 -0500

    comentando homepage

--- src/main/java/org/example/elements/pages/DemoBlazeHomePage.java
+++ src/main/java/org/example/elements/pages/DemoBlazeHomePage.java
@@ -2,11 +2,11 @@ package org.example.elements.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-public class DemoBlazeHomePage extends BasePage {
+public class DemoBlazeHomePage extends BasePage { //extiende de la clase BasePage porque es una pagina web y hereda sus metodos de inicializacion de elementos
     private By productLink = By.linkText("samsung galaxy s6"); //se crea un objeto By que contiene el link del producto
 
-    public DemoBlazeHomePage(WebDriver driver) {
-        super(driver);
+    public DemoBlazeHomePage(WebDriver driver) { //constructor de la clase
+        super(driver); //se llama al constructor de la clase padre que es BasePage y se le pasa el driver
 
     }
 
