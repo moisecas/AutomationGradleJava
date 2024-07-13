72d7ad021958a9df8232c9d93e7ecec6a2e66a20
unknown <moisesjuniorcas20@gmail.com>
2024-07-13 13:48:16 -0500

    comentando la explicación del test

--- src/test/java/steps/AddToCartSteps.java
+++ src/test/java/steps/AddToCartSteps.java
@@ -19,27 +19,27 @@ public class AddToCartSteps {
 
     @Given("the user is on the DemoBlaze home page") //se inicializa el driver de chrome y se abre la pagina de demoblaze
     public void the_user_is_on_the_demo_blaze_home_page() {
-        driver = WebDriverManagerUtil.getDriver();
-        driver.get("https://www.demoblaze.com/index.html");
-        homePage = new DemoBlazeHomePage(driver);
+        driver = WebDriverManagerUtil.getDriver(); //se inicializa el driver de chrome
+        driver.get("https://www.demoblaze.com/index.html"); //visita la pagina de demoblaze
+        homePage = new DemoBlazeHomePage(driver); //se crea un objeto de la clase DemoBlazeHomePage para poder acceder a sus metodos
     }
-    @When("the user navigates to the Samsung galaxy s6 product page")
+    @When("the user navigates to the Samsung galaxy s6 product page") //se navega a la pagina del producto Samsung galaxy s6
     public void the_user_navigates_to_the_samsung_galaxy_s6_product_page() {
-        homePage.goToProductPage();
-        productPage = new ProductPage(driver);
+        homePage.goToProductPage(); //homePage es un objeto de la clase DemoBlazeHomePage y se llama al metodo goToProductPage
+        productPage = new ProductPage(driver); //se crea un objeto de la clase ProductPage para poder acceder a sus metodos
     }
 
     @When("the user adds the product to the cart")
     public void the_user_adds_the_product_to_the_cart() {
-        productPage.addToCart();
-        cartPage = new CartPage(driver);
-        cartPage.goToCart();
+        productPage.addToCart(); //productPage es un objeto de la clase ProductPage y se llama al metodo addToCart
+        cartPage = new CartPage(driver); //se crea un objeto de la clase CartPage para poder acceder a sus metodos
+        cartPage.goToCart(); //se llama al metodo goToCart
     }
 
     @Then("the product should be added to the cart successfully")
     public void the_product_should_be_added_to_the_cart_successfully() {
-        assertTrue(cartPage.isProductInCart());
-        driver.quit();
+        assertTrue(cartPage.isProductInCart()); //se llama al metodo isProductInCart de la clase CartPage y se verifica que el producto este en el carrito
+        driver.quit(); //se cierra el driver de chrome para finalizar la prueba de aceptacion
     }
 }
 
