4bb945f1529d2091d9efa6b01a7e814080d2b44b
unknown <moisesjuniorcas20@gmail.com>
2024-07-13 14:22:57 -0500

    creando metodos para login

--- src/main/java/org/example/elements/pages/LoginPage.java
+++ src/main/java/org/example/elements/pages/LoginPage.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 public class LoginPage extends BasePage {
 
-    private By usernameField = By.id("loginusername");
-    private By passwordField = By.id("loginpassword");
+    private By usernameField = By.id("loginusername"); //se crea un objeto By que contiene el id del campo de usuario
+    private By passwordField = By.id("loginpassword"); //se crea un objeto By que contiene el id del campo de contraseña
     private By loginButton = By.xpath("//button[text()='Log in']");
 
     public LoginPage(WebDriver driver) {
--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -15,8 +15,24 @@ public class LoginSteps {
     public void the_user_is_on_the_demoblaze_login_page() {
         driver = WebDriverManagerUtil.getDriver();
         driver.get("https://www.demoblaze.com/index.html");
-        loginPage = new LoginPage(driver);
+        loginPage = new LoginPage(driver); //se crea un objeto de la clase LoginPage para poder acceder a sus metodos
     }
 
+    @When("the user enters valid credentials")
+    public void the_user_enters_valid_credentials() {
+        loginPage.enterUsername("testuser");
+        loginPage.enterPassword("testpassword"); //
+    }
+
+    @When("the user submits the login form")
+    public void the_user_submits_the_login_form() {
+        loginPage.submitLogin(); //loginPage es un objeto de la clase LoginPage y se llama al metodo submitLogin
+    }
+
+    @Then("the user should be logged in successfully")
+    public void the_user_should_be_logged_in_successfully() {
+        assertTrue(loginPage.isLoginSuccessful()); //asseertTrue verifica que el metodo isLoginSuccessful de la clase LoginPage retorne true
+        driver.quit();
+    }
 
 }
