8528f1c2d2462e6aaa1455984e1e666c2f114ee1
unknown <moisesjuniorcas20@gmail.com>
2024-08-18 15:20:02 -0500

    empezando a construir los steps

--- src/test/java/steps/FormContactSteps.java
+++ src/test/java/steps/FormContactSteps.java
@@ -1,5 +1,29 @@
 package steps;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import org.example.elements.utils.WebDriverManagerUtil;
+import org.openqa.selenium.WebDriver;
+import org.example.elements.pages.DemoBlazeHomePage;
+import org.example.elements.pages.ContactPage;
 public class FormContactSteps {
+    private WebDriver driver;
 
+    @Given("the user is on the DemoBlaze homepage")
+    public void go_to_home_page(){
+        driver = WebDriverManagerUtil.getDriver();
+        driver.get("https://www.demoblaze.com/index.html");
+
+    };
+
+    @When("the user navigates to the {string} section")
+    public void form_contact(){
+
+    };
+
+    @Then("the user should be able to fill out the contact form and submit it successfully")
+    public void form_contact_clic(){
+
+    };
 }
